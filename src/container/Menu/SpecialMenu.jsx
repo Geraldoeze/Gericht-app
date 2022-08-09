1596cff64bd7d81b74a897df83d841744c29e4df
Gerald Ezenagu <egerald344@gmail.com>
2022-08-09 22:11:23 +0100

    special menu done

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -11,11 +11,11 @@ const SpecialMenu = () => (
     </div>
 
     <div className='app__specialMenu-menu'>
-      <div className='app__specialMenu-menu_wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+      <div className='app__specialMenu-menu_wine flex__center '>
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
         <div className='app__specialMenu_items' >
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
@@ -23,6 +23,18 @@ const SpecialMenu = () => (
       <div className='app__specialMenu-menu_img'>
         <img src={images.menu} alt='menu img' />
       </div>
+
+      <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className='app__specialMenu-menu_heading'>Cocktails</p>
+        <div className='app__specialMenu_items' >
+          {data.cocktails.map((cock, index) => (
+            <MenuItem key={cock.title + index} title={cock.title} price={cock.price} tags={cock.tags} />
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );

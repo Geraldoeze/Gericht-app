b978d2faa80cc4617b127bb9001b4343cfb8142b
Gerald Ezenagu <egerald344@gmail.com>
2022-08-10 14:23:09 +0100

    working on gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { SubHeading, MenuItem } from '../../components';
+import {images, data} from '../../constants';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+
+
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title="Instagram" />
+      </div>
+    </div>
+  );
+}
 
 export default Gallery;
